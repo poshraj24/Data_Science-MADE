be30954936e463546dd4ba588cbe8bf2304bc523
Posh Raj Dahal <dahal.poshraj24@gmail.com>
2024-07-02 17:06:48 +0200

    minor fixes

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -1,6 +1,6 @@
 #installing the required packages and running the pipeline
 pip install --upgrade pip
-pip install -r ./requirements.txt
+pip install -r requirements.txt
 
 # Running the pipeline
 python ./pipeline.py
